8b1a27f27775e53beb6aeb33c70a8841b7bb1a40
Chris Roberts <chrisrobertscollective@gmail.com>
2024-09-17 12:12:21 +0200

    creating left side bar

--- components/DoughnutChart.tsx
+++ components/DoughnutChart.tsx
@@ -1,12 +1,7 @@
 'use client'
 import React from 'react'
 import { Doughnut } from 'react-chartjs-2';
-import {
-  Chart as ChartJS,
-  ArcElement,
-  Tooltip,
-  Legend,
-} from 'chart.js';
+import { Chart as ChartJS, ArcElement, Tooltip, Legend,   } from 'chart.js';
 
 ChartJS.register(ArcElement, Tooltip, Legend);
 
