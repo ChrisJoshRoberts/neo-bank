c06583fa640e173894e1085f3ccc465d98f6515b
Chris Roberts <chrisrobertscollective@gmail.com>
2024-09-24 14:37:16 +0200

    implementing appwrite

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -1,5 +1,10 @@
 'use server'
 
+import { ID } from 'node-appwrite'
+import { createSessionClient, createAdminClient } from '../appwrite'
+import { cookies } from 'next/headers'
+import { parseStringify } from '../utils'
+
 export const signIn = async () => {
   try {
 
@@ -11,7 +16,25 @@ export const signIn = async () => {
 export const signUp = async (userData: SignUpParams) => {
   try {
     // Create a user account
+    const { account } = await createAdminClient();
+    const newUserAccount = await account.create(ID.unique(), userData.email, userData.password, `${userData.firstName} ${userData.lastName}`);
+    const session = await account.createEmailPasswordSession(userData.email, userData.password);
+    cookies().set("appwrite-session", session.secret, {
+      path: "/",
+      httpOnly: true,
+      sameSite: "strict",
+      secure: true,
+    });
+    return parseStringify(newUserAccount);
   } catch (error) {
     console.error("Error", error)
+  }
+}
+export async function getLoggedInUser() {
+  try {
+    const { account } = await createSessionClient();
+    return await account.get();
+  } catch (error) {
+    return null;
   }
 }
